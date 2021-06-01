ea63133c4993dddd25dd0d573a45ad97d263ae08
Jeremy Seckinger <jtseck@aol.com>
2021-06-01 14:56:28 -0700

    Added comment on config passport callback url

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports = function(passport) {  //passport brought in from server.js
     passport.use(new GoogleStrategy({
         clientID : process.env.GOOGLE_CLIENT_ID,
         clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'https://traveldashboard.herokuapp.com/auth/google/callback'
+        callbackURL: 'https://traveldashboard.herokuapp.com/auth/google/callback'  //added full callback URL when deploying to heroku otherwise it wasn't working as https 
     },
     async (accessToken, refreshToken, profile, done) => { // using async await since dealing with mongoose  done is callback when finished doing what we intend
         const newUser = { //creating new user from google profile on signin
